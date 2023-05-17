2488db84e6610707ddda7b6fdccf91cd83b6a0ec
Christoffer Jepsen <Christoffer.ssj@gmail.com>
2023-05-17 15:00:14 +0200

    added docker installs and post docker install changes

--- customsetup.sh
+++ customsetup.sh
@@ -2,7 +2,13 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt -y install vim i3 python3-pip autojump git htop
+sudo apt -y install vim i3 python3-pip autojump git htop docker docker-compose
+
+# Docker post install
+sudo groudapp docker
+sudo usermod -aG docker $USER
+
+newgrp docker
 
 
 pip3 install ipython
