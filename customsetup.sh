81283d0883ad42f364984dd23d2cd78cf7e831e7
TRiBByX <christoffer.ssj@gmail.com>
2023-10-26 13:09:47 +0200

    Formattet comments to correctly add newlines. Also added som terminal flavour.

--- customsetup.sh
+++ customsetup.sh
@@ -2,15 +2,15 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt -y install vim i3 python3-pip autojump git htop docker docker-compose net-tools
+sudo apt -y install vim i3 python3-pip autojump git htop docker docker-compose net-tools exif
 
 # Docker post install
 sudo groudadd docker
 sudo usermod -aG docker $USER
 
 pip3 install ipython
-echo "# Custom settings - Added by setup script." >> ~/.bashrc
+
+echo -e "\n\n# Custom settings - Added by setup script." >> ~/.bashrc
 echo "alias apt='apt -y'" >> ~/.bashrc
 echo ". /usr/share/autojump/autojump.sh" >> ~/.bashrc
-
-
+echo "PS1=\"\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1\"" >> ~/.bashrc
